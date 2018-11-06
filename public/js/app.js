99878aabc5619833214e9fcaff7be9859f0acc15
julianamtyler <julianam.tyler@gmail.com>
2018-11-05 20:48:28 -0500

    adds to db and displays after refresh

--- public/js/app.js
+++ public/js/app.js
@@ -1,41 +1,42 @@
 $(function () {
 
-//adds items in database but does not show it in console until refresh
 
-    const todoList = function () {
-        $('#todoitems').empty();
-        $.ajax({ url: '/api/todoItems', method: 'GET' })
-            .then(function (todoItems) {
-                console.log(todoItems)
-            });
-    }
+const todoList = function () {
+    $('#todoitems').empty();
+    $.ajax({ url: '/api/todoItems', method: 'GET' })
+        .then(function (todoList) {
+            todoList.forEach(e => {$(`#todoItems`).append($(`<li class="list"><input class="checkbox" type="checkbox"/><span><b>${e.todoname}</b></span><i class="fas fa-times"></li></div>`))})
+            console.log(todoList)
+        });
+}
+todoList();
 
-    todoList();
     const postItem = function (newtodo) {
         $.ajax({ url: '/api/todoItems', method: 'POST', data: newtodo })
             .then(function (todoItems) {
+                console.log('hello there')
                 let todoname = $('#todo').val();
-                $('#todoItems').append($(`<div><input class="checkbox" type="checkbox"/>${todoname}<i class="fas fa-times"></i></div>`))
+                $('#todoItems').append($(`<li class="list"><input class="checkbox" type="checkbox"/><span><b>${todoname}</b></span><i class="fas fa-times"></i></li>`))
             })
 
 
         $('#todoItems')
     }
 
-    const remove = function (remove) {
-        $.ajax({ url: '/api/todoItems', method: 'DELETE', data: remove })
-            .then(function () {
-                // $('#fa-times').on('click', function () {
-                //     remove();
-                // })
-                $('fa-times').on('click', function(){
-                    $(this).remove()
-                })
-            })
-    }
+    // const remove = function (remove) {
+    //     $.ajax({ url: '/api/todoItems', method: 'DELETE', data: remove })
+    //         .then(function () {
+    //             // $('#fa-times').on('click', function () {
+    //             //     remove();
+    //             // })
+    //             $('fa-times').on('click', function(){
+    //                 $(this).remove()
+    //             })
+    //         })
+    // }
 
-    $('#submit').on('click', function () {
-
+    $('#submit').on('click', function (event) {
+        event.preventDefault();
         const val = { todoname: $('#todo').val(), done: false }
         postItem(val)
 
